414ad22cf7890cdfd9a123750023a2499849ec10
deeandra <deesetyaptr@gmail.com>
2023-06-27 05:42:28 +0700

    add catch after throw in server.js

--- src/routes/logout/+server.js
+++ src/routes/logout/+server.js
@@ -2,11 +2,18 @@ import { error, redirect } from '@sveltejs/kit'
 
 export async function POST({ locals }) {
 
-    const {error: err} = await locals.supabase.auth.signOut()
+    try {
+        const {error: err} = await locals.supabase.auth.signOut()
 
-    if (err) {
-        throw error(500, 'Something went wrong logging you out.')
+        if (err) {
+            throw error(500, 'Something went wrong logging you out.')
+        }
+    
+    }
+    catch(error) {
+        console.log(error)
     }
 
+  
     throw redirect(303, '/')
 }
